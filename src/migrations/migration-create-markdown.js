b71632408c413f429f30a8195ca96642c6ed561a
giangnguyenbkhn <nguyengiang20112001@gmail.com>
2023-05-17 16:54:27 +0700

    infor details doctor  with markdown editor (1)

---
+++ src/migrations/migration-create-markdown.js
@@ -0,0 +1,51 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("markdowns", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      contentHTML: {
+        allowNull: false,
+        type: Sequelize.TEXT("long"),
+      },
+      contentMarkdown: {
+        allowNull: false,
+        type: Sequelize.TEXT("long"),
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.TEXT("long"),
+      },
+      doctorId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      specialtyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      clinicId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("markdowns");
+  },
+};
+// Migration cho phép bạn định nghĩa các bảng trong CSDL,
+//định nghĩa nội dung các bảng cũng như cập nhật thay đổi các bảng đó
